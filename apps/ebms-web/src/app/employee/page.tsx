1724730af57a94980521f52e1a84dca6e813d9ac
Travis <gantushig69@gmail.com>
2026-03-10 15:28:11 +0800

    header

--- apps/ebms-web/src/app/employee/page.tsx
+++ apps/ebms-web/src/app/employee/page.tsx
@@ -1,8 +1,10 @@
 import { NewRequest } from "./components/benefit_req";
+import { Header } from "./components/Header";
 
 export default function EmployeePage() {
     return (
         <div className="w-full h-full bg-[#0f172a]">
+            <Header/>
             <NewRequest />
         </div>
     )
